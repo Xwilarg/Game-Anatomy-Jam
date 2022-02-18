7d2babdbf8cfffd7c4a4ec45284387e8b36cb907
Amaya <guillaume.le-berre@epitech.eu>
2022-02-18 18:41:31 -0500

    Add image translation

---
+++ Assets/Scripts/MiniGames/BarTranslation.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Minigame
+{
+    public class BarTranslation : MonoBehaviour
+    {
+        private RectTransform _rect;
+
+        private float _offset;
+
+        [SerializeField]
+        private RectTransform _canvas;
+
+        private void Start()
+        {
+            _rect = (RectTransform)transform;
+            _offset = _canvas.sizeDelta.x / 20f;
+        }
+
+        private void FixedUpdate()
+        {
+            var lengthCanvas = _canvas.sizeDelta.x / 2f;
+
+            if (_rect.localPosition.x - (_rect.sizeDelta.x / 2f) < -1f * lengthCanvas)
+            {
+                _offset = lengthCanvas / 50f;
+            }
+            else if (_rect.localPosition.x + (_rect.sizeDelta.x / 2f) > lengthCanvas)
+            {
+                _offset = (lengthCanvas / 50f) * -1f;
+            }
+
+            _rect.localPosition = new(
+                _rect.localPosition.x + _offset,
+                _rect.localPosition.y
+            );
+        }
+
+        public void OnAction(InputAction.CallbackContext value)
+        {
+            if (value.performed)
+            {
+                _rect.localPosition = new(0, 0);
+            }
+        }
+    }
+}
