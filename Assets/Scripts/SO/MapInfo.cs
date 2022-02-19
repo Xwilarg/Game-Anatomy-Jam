f65a76c3f1c683f3908c92d7fc0aaa1a31f878bc
Xwilarg <xwilarg@yahoo.fr>
2022-02-19 01:16:20 +0100

    Add terrain scrolling

---
+++ Assets/Scripts/SO/MapInfo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace AnatomyJam.SO
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/MapInfo", fileName = "MapInfo")]
+    public class MapInfo : ScriptableObject
+    {
+        /// <summary>
+        /// Size of the pixel of a tile
+        /// </summary>
+        public float PixelSize;
+        /// <summary>
+        /// Number of line a group of tile contain
+        /// </summary>
+        public int NbOfLines;
+        /// <summary>
+        /// Scrolling speed
+        /// </summary>
+        public float ScrollingSpeed;
+
+        /// <summary>
+        /// Y value before object is moved on top
+        /// </summary>
+        public float MinBeforeReset;
+    }
+}
