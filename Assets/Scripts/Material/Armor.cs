0f0502fa6de3ed85d7cefbace160e279c375023a
Xwilarg <xwilarg@yahoo.fr>
2022-02-20 17:18:30 +0100

    Add Armor script

---
+++ Assets/Scripts/Material/Armor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace AnatomyJam.Material
+{
+    public class Armor : MonoBehaviour
+    {
+        [SerializeField]
+        private int _value;
+
+        public int Value => _value;
+    }
+}
