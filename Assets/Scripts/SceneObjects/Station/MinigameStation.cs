cbfe0ad95b456d556d74b1d645704bdf78270e5c
Kenan Lejosne <kenan.lejosne@gmail.com>
2022-02-20 09:54:02 +0100

    Add l̵̠͙͍̭͙̤̲͔̒͐̇̽̏͋̚a̶̧̧̛̛͉̣̹̩̖̱̾̊̌̒̉̏̃͗͒̔m̶͓͈̮̾̄̿̿͒̈́̌̏̈́͐̽̌̚͝͝b̷̤͔͓̫̪͙̟̙́̊̐̆̚͠d̵̩͛̌͝á̸̛̱̤̩͎͉̰̂͛̊̈́̐̽͘ ̸̡͉̖̙̭͈̯̈̏̔̃̔̿̀͑̂͗̚͠c̸̛̪̪͙̮͌̀̀̔̓̊͊̅̚͜a̷̹͓̥͙̝̿̋̑̔̾̿̓͒͐̿͌̚l̸̨̛̀́͊̈̒͐̾̆̒̅̆̎̕l̸̨̧̡̗̬̤̯̹͙͚͍̣͕̾̌̌͒̌̎͑̕͘̕͝͠͝ḅ̴̛̠̮͍̞̥͕̦̼̒͐̓̈́ả̴̬̏̇̃̐̏͐͗̋̕c̷̛̳̭̠͎̩̎̾̅͑̽͑̿͘͠k̷̻̱̆̌̓̅̓ ̴͚̣̖̰̃̄̑̽̔̍̊̈̄̀̃̀̈́͌̕h̸̪͓̠̫͖̖̗̯͕̲̞͕̉̊̄͜ë̶͖͙́̾̎́̋̒͑̽̎ļ̵̢̰̰̤̣̮͓̯̼̱͇̮̊̐͒́̓̊͌͝l̴͉͇͍̻͕̲̰͖̦̺̣͌̾̾̽͂̑͘͜

--- Assets/Scripts/SceneObjects/Station/MinigameStation.cs
+++ Assets/Scripts/SceneObjects/Station/MinigameStation.cs
@@ -1,18 +1,31 @@
 ﻿using AnatomyJam.Player;
+using UnityEngine;
+using System;
 
 namespace AnatomyJam.SceneObjects.Station
 {
     public class MinigameStation : AStation
     {
+        [SerializeField]
+        private GameObject minigame;
         public override void Deposit(PlayerController pc, SceneObject obj)
         {
             var result = GetRecipe(obj);
             pc.CanMove = false;
+
+            Minigame.MinigameCallBack cb = () =>
+            {
+                pc.CanMove = true;
+                ThrowOnFloor(pc, obj, result);
+            };
+
+            minigame.GetComponent<Minigame.AMiniGameManager>()?.RunMinigame(cb, 1);
             // TODO: Launch minigame
             // TODO: Once minigame is complete, call:
             // pc.CanMove = true;
             // ThrowOnFloor(pc, obj, result);
         }
+
     }
 }
 
