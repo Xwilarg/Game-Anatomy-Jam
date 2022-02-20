c5ebe5f829d45f6c29a752980ce62f7dcb4279e5
Kenan Lejosne <kenan.lejosne@gmail.com>
2022-02-20 11:52:35 +0100

    Add difficulty for quenching

--- Assets/Scripts/MiniGames/AMiniGameManager.cs
+++ Assets/Scripts/MiniGames/AMiniGameManager.cs
@@ -5,7 +5,7 @@ namespace Minigame
     public delegate void MinigameCallBack();
     public abstract class AMiniGameManager : MonoBehaviour
     {
-        public virtual void RunMinigame(MinigameCallBack cb_result, int difficultyFactor)
+        public virtual void RunMinigame(MinigameCallBack cb_result, float difficultyFactor)
         {
             gameObject.SetActive(true);
         }
