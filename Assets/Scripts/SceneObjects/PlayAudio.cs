e4ee35907cdf2ce5fb8c271c8d4174c33218f1d4
Xwilarg <xwilarg@yahoo.fr>
2022-02-20 22:07:41 +0100

    Add chest audio

---
+++ Assets/Scripts/SceneObjects/PlayAudio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace AnatomyJam.SceneObjects
+{
+    public class PlayAudio : MonoBehaviour
+    {
+        private AudioSource _source;
+
+        private void Start()
+        {
+            _source = GetComponent<AudioSource>();
+        }
+
+        public void Play()
+        {
+            _source.Play();
+        }
+    }
+}
